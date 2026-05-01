7852b9ed17d46d1d8af8d6fac239463af65a3581
Hung Do <dophuochung2428@gmail.com>
2026-05-01 20:42:12 +0700

    fix(map): resolve marker icon display issue

--- components/Map.tsx
+++ components/Map.tsx
@@ -10,7 +10,18 @@ import {
 } from "react-leaflet";
 import { useEffect } from "react";
 import { Property } from "../types/property";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
+// fix icon path
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
 
 const { BaseLayer } = LayersControl;
 
