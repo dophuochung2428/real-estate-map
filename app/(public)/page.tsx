44bc30ee8a1fb492731ece2602a308cddd446173
Hung Do <dophuochung2428@gmail.com>
2026-05-11 17:43:45 +0700

    fix(db): rv log error

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -15,8 +15,6 @@ import MobileBottomNav from "@/components/mobile/mobile-bottom-nav";
 import FloatingActions from "@/components/common/floating-actions";
 
 export default function HomePage() {
-  console.log("DATABASE_URL:", process.env.DATABASE_URL);
-  console.log("APP_URL:", process.env.NEXT_PUBLIC_APP_URL);
   return (
     <main className="bg-[var(--background)] pb-24 lg:pb-0">
       <MainHeader />
